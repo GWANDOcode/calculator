d9b701511a9e11066549e5af102cb885c9620d56
GWANDO.code <118937578+GWANDOcode@users.noreply.github.com>
2023-03-06 16:51:39 +0100

    Add basic operators like addition

---
+++ script.js
@@ -0,0 +1,15 @@
+const addition = function(add1, add2) {
+   return add1 + add2;
+};
+
+const subtraction = function(sub1, sub2) {
+    return sub1 - sub2;
+};
+
+const multiplication = function(mul1, mult2) {
+    return mul1 * mult2;
+}
+
+const division = function(div1, div2) {
+    return div1 / div2;
+}
