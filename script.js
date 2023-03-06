60b4ff6b3b9bfbae47b6d769ffe3b5ae9a5a6ab1
GWANDO.code <118937578+GWANDOcode@users.noreply.github.com>
2023-03-06 16:57:21 +0100

    Add mathHandler
    Takes 2 numbers and an operator, return restult.

--- script.js
+++ script.js
@@ -8,8 +8,12 @@ const subtraction = function(sub1, sub2) {
 
 const multiplication = function(mul1, mult2) {
     return mul1 * mult2;
-}
+};
 
 const division = function(div1, div2) {
     return div1 / div2;
-}
+};
+
+const mathHandler = function(num1, operator, num2) {
+    return operator(num1, num2);
+};
